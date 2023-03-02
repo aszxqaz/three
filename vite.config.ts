9637a97cda1685bb39ccc0f6c57bd580b8e812fe
Maxim B <b.maxim.dev@gmail.com>
2023-03-03 03:37:42 +0400

    fixed vite.config and start script for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 3003
-}
+  plugins: [react()]
 })
